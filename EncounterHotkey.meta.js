440d89e9af7cbe99d0ef8248c771d2f6938a770e
linbilly <billy.lin@gmail.com>
2015-03-07 10:15:23 -0800

    updated comments for public release

--- EncounterHotkey.meta.js
+++ EncounterHotkey.meta.js
@@ -3,7 +3,7 @@
 // @namespace   http://dev.drbillylin.com/oscarwiki
 // @description	Listens for hotkeys and do oscar functions. Eg. entering BP measurement when user types "BP 120/90".
 // @include     */casemgmt/forward.jsp?action=view*
-// @version     1
+// @version     1.1
 // @grant       none
 // @author		Billy Lin
 // @updateURL	https://github.com/linbilly/EncounterHotkey/raw/master/EncounterHotkey.meta.js
--- EncounterHotkey.user.js
+++ EncounterHotkey.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://dev.drbillylin.com/oscarwiki
 // @description	Listens for hotkeys and do oscar functions. Eg. entering BP measurement when user types "BP 120/90".
 // @include     */casemgmt/forward.jsp?action=view*
-// @version     1
+// @version     1.1
 // @grant       none
 // @author		Billy Lin
 // @updateURL	https://github.com/linbilly/EncounterHotkey/raw/master/EncounterHotkey.meta.js
@@ -15,79 +15,70 @@ var oldvalue;
 var cursorLocation;
 
 window.addEventListener("load",function(){
-	// console.log("KeyListner working");
-	
 	setTimeout(function(){
 
 		activeNote = document.getElementsByName("caseNote_note")[0];
 		activeNote.addEventListener("keypress", findString);
-		//activeNote.onkeyup=findString;
 
-		// console.log(activeNote);
-		// console.log("listener added");
 		function findString(e){
 			evt = e || window.event; // compliant with ie6     
 
 			//if the user presses "Enter", proceed
 			if (evt.keyCode == 13) { 
-	        	// console.log('Enter Pressed');
-
-				// var content = activeNote.value;
 				cursorLocation = activeNote.selectionStart;
 				oldvalue = activeNote.value;
 				var previousLines = activeNote.value.substring(0, cursorLocation).split('\n');
 
 				var lastLine = previousLines[previousLines.length-1];
-				// console.log(lastLine);
-
-				//if there is <BP 120/80> in the last line:
-				var bpRegex = /BP\s*(\d+)\/(\d+)/gi;
 				var myArray;
+
+				// regex to find <BP 120/80> in the last line:
+				var bpRegex = /BP\s*(\d+)\/(\d+)/gi;
 
 				if((myArray=bpRegex.exec(lastLine))!==null){
 					enterBPMeasurement(myArray);
 				}
+
+				// THIS IS WHERE NEW REGEX CAN BE ADDED ALONG WITH NEW METHODS BELOW
+				// var newRegex = /some regex/gi;
+				// if((myArray = newRegex.exec(lastLine))!==null){
+				// 	dosomething(myArray);
+				// }
 			}
 		}
 	},1000);
 }, false);
 
+//Method to enter BP Measurement automatically. Regex for the matchedArray is /BP\s*(\d+)\/(\d+)/gi
 function enterBPMeasurement(matchedArray){
-	// console.log(myArray);
 	var sbp=matchedArray[1];
 	var dbp=matchedArray[2];
-	// console.log("BP is: "+sbp+"/"+dbp);
 
 	// enter BP value https://../../oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=BP
 	var pathArray = window.location.pathname.split( '/' );
 	var newURL = window.location.protocol + "//" + window.location.host +"/"+pathArray[1]+"/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=BP";
-	// console.log(newURL);
 	var measurementWindow = window.open(newURL);
-	// console.log(measurementWindow);
 
 	measurementWindow.addEventListener("load", function(){
-		// console.log(measurementWindow);
+		// putting the BP value in the correct input box
 		var bpInputBox = measurementWindow.document.getElementsByName("value(inputValue-0)")[0];
-		// console.log(bpInputBox);
 		bpInputBox.value = sbp+'/'+dbp;
 
+		// finding the submit button and clicking on it
 		var measurementButtons = measurementWindow.document.getElementsByName("Button");
-		// console.log(measurementButtons);
 		for(var i=0; i<measurementButtons.length; i++){
 			var mButton = measurementButtons[i];
-			// console.log(mButton);
 			if(mButton.getAttribute('value')=="Submit"){
-				// while(bpInputBox.value!==sbp+'/'+dbp){
-				// 	// console.log(bpInputBox.value);
-				// }
 				mButton.click();
 			}
 		}
 	});
 
+	// Some time after clicking on submit, the measurement window would close. As it closes, it will append a BP reading at the end of the current active note
+	// The code below remove the OSCAR generated BP reading at the end of the active note in favor of keeping the user typed BP value in its original place
 	var timer = setInterval(function(){
+		//after the measurement window closes, 
 		if(measurementWindow.closed){
-			// console.log(cursorLocation);
 			activeNote.value = [oldvalue.slice(0, cursorLocation),'\n',oldvalue.slice(cursorLocation)].join('');
 			activeNote.focus();
 			activeNote.setSelectionRange(cursorLocation+1,cursorLocation+1);
@@ -95,3 +86,8 @@ function enterBPMeasurement(matchedArray){
 		}
 	},100);
 }
+
+// ADD NEW FUNCTIONS HERE FOR MORE HOTKEYS ACTIONS
+//function dosomething(matchedArray){
+
+// }
