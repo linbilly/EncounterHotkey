45912b038d8b465bc4bbcc6ae842ef9b80ad6234
linbilly <linbilly@users.noreply.github.com>
2026-02-11 20:38:37 -0800

    Update EncounterHotkey.user.js
    Added a few vital signs to be entered via hotkey

--- EncounterHotkey.user.js
+++ EncounterHotkey.user.js
@@ -1,14 +1,16 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        EncounterHotkey
 // @namespace   http://dev.drbillylin.com/oscarwiki
 // @description	Listens for hotkeys and do oscar functions. Eg. entering BP measurement when user types "BP 120/90".
 // @include     */casemgmt/forward.jsp?action=view*
-// @version     1.2
+// @version     1.2026
 // @grant       none
 // @author		Billy Lin
 // @updateURL	https://github.com/linbilly/EncounterHotkey/raw/master/EncounterHotkey.meta.js
 // @downloadURL	https://github.com/linbilly/EncounterHotkey/raw/master/EncounterHotkey.user.js
 // ==/UserScript==
+
+// The BP measurement group needs to be created first for this to work. 
 
 var activeNote;
 var oldvalue;
@@ -16,27 +18,28 @@ var cursorLocation;
 
 window.addEventListener("load",function(){
 
+
 	// Insert iFrame to hide the windows
 	var myframe = document.createElement("iframe");
 	myframe.name = "hiddenWin";
 	myframe.id = "hiddenWin";
 	myframe.height="0px";
 	myframe.width="0px";
-	// myframe.setAttribute("hidden",true);
 	document.getElementsByTagName("body")[0].appendChild(myframe);
-
-
 
 	setTimeout(function(){
 
 		activeNote = document.getElementsByName("caseNote_note")[0];
 		activeNote.addEventListener("keypress", findString);
 
+
 		function findString(e){
-			evt = e || window.event; // compliant with ie6     
+
+			const evt = e || window.event; // compliant with ie6     
 
 			//if the user presses "Enter", proceed
 			if (evt.keyCode == 13) { 
+
 				cursorLocation = activeNote.selectionStart;
 				oldvalue = activeNote.value;
 				var previousLines = activeNote.value.substring(0, cursorLocation).split('\n');
@@ -44,36 +47,67 @@ window.addEventListener("load",function(){
 				var lastLine = previousLines[previousLines.length-1];
 				var resultArray;
 
-				// regex to find <BP 120/80> in the last line:
-				var bpRegex = /BP\s*(\d+)\/(\d+)/gi;
-
+				var bpRegex = /BP\s*(\d+\/\d+)/gi;
 				if((resultArray=bpRegex.exec(lastLine))!==null){
-					enterBPMeasurement(resultArray);
+					enterBPMeasurement(resultArray[1],"sitting position");
 				}
 
-				// THIS IS WHERE NEW REGEX CAN BE ADDED ALONG WITH NEW METHODS BELOW
-				// var newRegex = /some regex/gi;
-				// if((resultArray = newRegex.exec(lastLine))!==null){
-				// 	dosomething(resultArray);
-				// }
+				var supinebpRegex = /Supine BP\s*(\d+\/\d+)/gi;
+				if((resultArray=supinebpRegex.exec(lastLine))!==null){
+					enterBPMeasurement(resultArray[1],"supine");
+				}
+
+				var standingbpRegex = /Standing BP\s*(\d+\/\d+)/gi;
+				if((resultArray=standingbpRegex.exec(lastLine))!==null){
+					enterBPMeasurement(resultArray[1],"standing position");
+				}
+
+				var standingbpRegex = /BP Tru\s*(\d+\/\d+)/gi;
+				if((resultArray=standingbpRegex.exec(lastLine))!==null){
+					enterBPMeasurement(resultArray[1],"BP Tru");
+				}
+
+
+				var wtRegex = /wt\s*(\d+)\s*kg/gi;
+				if((resultArray = wtRegex.exec(lastLine))!==null){
+					enterMeasurement(resultArray[1],"inputValue-2")
+				}
+
+				var tempRegex = /temp\s*(\d+)/gi;
+				if((resultArray = tempRegex.exec(lastLine))!==null){
+					enterMeasurement(resultArray[1],"inputValue-3")
+				}
+
+				var cbgRegex = /cbg\s*(\d+(\.\d+)?)\s*mmol\/L/gi;
+				if((resultArray = cbgRegex.exec(lastLine))!==null){
+					enterMeasurement(resultArray[1],"inputValue-1")
+				}
+
+				var rrRegex = /rr\s*(\d+)/gi;
+				if((resultArray = rrRegex.exec(lastLine))!==null){
+					enterMeasurement(resultArray[1],"inputValue-5")
+				}
+
+				var hrRegex = /hr\s*(\d+)/gi;
+				if((resultArray = hrRegex.exec(lastLine))!==null){
+					enterMeasurement(resultArray[1],"inputValue-6")
+				}
+
 			}
 		}
-	},1000);
+	},2000);
 }, false);
 
-//Method to enter BP Measurement automatically. Regex for the matchedArray is /BP\s*(\d+)\/(\d+)/gi
-function enterBPMeasurement(matchedArray){
-	var sbp=matchedArray[1];
-	var dbp=matchedArray[2];
+function enterMeasurement(mValue, boxLocation){
 
-	// enter BP value https://../../oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=BP
+	console.log(mValue, boxLocation); 
 	var pathArray = window.location.pathname.split( '/' );
-	var newURL = window.location.protocol + "//" + window.location.host +"/"+pathArray[1]+"/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=BP";
+	var newURL = window.location.protocol + "//" + window.location.host +"/"+pathArray[1]+"/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Patient%20Vital%20Measurements";
 	var measurementWindow = window.open(newURL, "hiddenWin");
 
 	document.getElementById("hiddenWin").onload = function(){
-		var bpInputBox = document.getElementById("hiddenWin").contentDocument.getElementsByName("value(inputValue-0)")[0];
-		bpInputBox.value = sbp+'/'+dbp;
+		var inputbox = document.getElementById("hiddenWin").contentDocument.getElementsByName("value("+boxLocation+")")[0];
+		inputbox.value = mValue;
 
 		// finding the submit button and clicking on it
 		var measurementButtons = document.getElementById("hiddenWin").contentDocument.getElementsByName("Button");
@@ -87,38 +121,48 @@ function enterBPMeasurement(matchedArray){
 					activeNote.focus();
 					activeNote.setSelectionRange(cursorLocation+1,cursorLocation+1);
 					clearInterval(timer);
-				},500);
+				},1000);
 			}
 		}
 	};
 
-	// measurementWindow.addEventListener("load", function(){
-	// 	// putting the BP value in the correct input box
-	// 	var bpInputBox = measurementWindow.document.getElementsByName("value(inputValue-0)")[0];
-	// 	bpInputBox.value = sbp+'/'+dbp;
-
-	// 	// finding the submit button and clicking on it
-	// 	var measurementButtons = measurementWindow.document.getElementsByName("Button");
-	// 	for(var i=0; i<measurementButtons.length; i++){
-	// 		var mButton = measurementButtons[i];
-	// 		if(mButton.getAttribute('value')=="Submit"){
-	// 			mButton.click();
-	// 		}
-	// 	}
-	// });
-
-	// Some time after clicking on submit, the measurement window would close. As it closes, it will append a BP reading at the end of the current active note
-	// The code below remove the OSCAR generated BP reading at the end of the active note in favor of keeping the user typed BP value in its original place
-	// var timer = setInterval(function(){
-	// 	//after the measurement window closes, 
-	// 		activeNote.value = [oldvalue.slice(0, cursorLocation),'\n',oldvalue.slice(cursorLocation)].join('');
-	// 		activeNote.focus();
-	// 		activeNote.setSelectionRange(cursorLocation+1,cursorLocation+1);
-	// 		clearInterval(timer);
-	// },1000);
 }
 
-// ADD NEW FUNCTIONS HERE FOR MORE HOTKEYS ACTIONS
-//function dosomething(matchedArray){
+//Method to enter BP Measurement automatically. Regex for the matchedArray is /BP\s*(\d+)\/(\d+)/gi
+function enterBPMeasurement(bpReading, typeButton){
 
-// }
+
+	var pathArray = window.location.pathname.split( '/' );
+	var newURL = window.location.protocol + "//" + window.location.host +"/"+pathArray[1]+"/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Patient%20Vital%20Measurements";
+	var measurementWindow = window.open(newURL, "hiddenWin");
+
+	document.getElementById("hiddenWin").onload = function(){
+		var bpInputBox = document.getElementById("hiddenWin").contentDocument.getElementsByName("value(inputValue-0)")[0];
+		bpInputBox.value = bpReading; 
+
+		var typeButtons = document.getElementById("hiddenWin").contentDocument.getElementsByName("value(inputMInstrc-0)"); 
+		for(var i=0; i<typeButtons.length; i++){
+			var tButton = typeButtons[i];
+			if(tButton.getAttribute('value')==typeButton){
+				tButton.checked = true; 
+			}
+		}
+			
+
+		// finding the submit button and clicking on it
+		var measurementButtons = document.getElementById("hiddenWin").contentDocument.getElementsByName("Button");
+		for(var i=0; i<measurementButtons.length; i++){
+			var mButton = measurementButtons[i];
+			if(mButton.getAttribute('value')=="Submit"){
+				mButton.click();
+				var timer = setInterval(function(){
+				//after the measurement window closes, 
+					activeNote.value = [oldvalue.slice(0, cursorLocation),'\n',oldvalue.slice(cursorLocation)].join('');
+					activeNote.focus();
+					activeNote.setSelectionRange(cursorLocation+1,cursorLocation+1);
+					clearInterval(timer);
+				},1000);
+			}
+		}
+	};
+}
